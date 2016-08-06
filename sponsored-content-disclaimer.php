bb97e84cf85f67b7c04bee1a8997f89dad1f71da
John Lane <tech@citymomsblog.com>
2016-08-06 15:39:58 -0400

    Initial checkin.

---
+++ sponsored-content-disclaimer.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Sponsored Content Disclaimer
+Plugin URI: http://www.citymomsblog.com
+Description: Display consistent sponsored content messages to a reader.
+Version: 0.1
+Author: City Moms Blog Network
+Author URI: http://www.citymomsblog.com
+License: GPLv2 or later
+*/
+
+// don't load directly
+if (!defined('ABSPATH')) die('-1');
