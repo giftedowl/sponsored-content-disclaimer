d40d3554708545eb4f472dfc36a2a1041942ecda
John Lane <tech@citymomsblog.com>
2016-08-07 12:30:29 -0400

    Added meta data box to Post types. Post meta includes a disclaimer message and display location (above/below post). Saving of disclaimer works, but above/below not working.

--- sponsored-content-disclaimer.php
+++ sponsored-content-disclaimer.php
@@ -3,7 +3,7 @@
 Plugin Name: Sponsored Content Disclaimer
 Plugin URI: http://www.citymomsblog.com
 Description: Display consistent sponsored content messages to a reader.
-Version: 0.1
+Version: 0.2
 Author: City Moms Blog Network
 Author URI: http://www.citymomsblog.com
 License: GPLv2 or later
@@ -11,3 +11,100 @@ License: GPLv2 or later
 
 // don't load directly
 if (!defined('ABSPATH')) die('-1');
+
+function scd_enqueue_admin_scripts() {
+  wp_enqueue_style( 'scd_admin_styles', plugins_url( 'css/admin.css', __FILE__ ) );
+}
+add_action( 'admin_enqueue_scripts', 'scd_enqueue_admin_scripts' );
+
+/* Fire our meta box setup function on the post editor screen. */
+add_action( 'load-post.php', 'scd_post_meta_boxes_setup' );
+add_action( 'load-post-new.php', 'scd_post_meta_boxes_setup' );
+
+/* Meta box setup function. */
+function scd_post_meta_boxes_setup() {
+
+  /* Add meta boxes on the 'add_meta_boxes' hook. */
+  add_action( 'add_meta_boxes', 'scd_add_post_meta_boxes' );
+
+  /* Save post meta on the 'save_post' hook. */
+  add_action( 'save_post', 'scd_save_post_class_meta', 10, 2 );
+}
+
+/* Create one or more meta boxes to be displayed on the post editor screen. */
+function scd_add_post_meta_boxes() {
+
+  add_meta_box(
+    'scd-post-class',      // Unique ID
+    'Sponsored Content Disclaimer',    // Title
+    'scd_post_class_meta_box',   // Callback function
+    'post',         // Admin page (or post type)
+    'normal'         // Context
+    );
+}
+
+/* Display the post meta box. */
+function scd_post_class_meta_box( $object, $box ) {
+
+  wp_nonce_field( basename( __FILE__ ), 'scd-post-class-nonce' );
+
+  ?>
+  <textarea name="scd-post-message" id="scd-post-message"><?php echo esc_attr( get_post_meta( $object->ID, 'scd-post-message', true ) ); ?></textarea> <br/>
+
+ <?php
+    $post_meta_display = get_post_meta($post->ID, 'scd-post-display', true);
+  ?>
+  <label for="scd-post-class"><?php _e( "Display:", 'example' ); ?></label><br/>
+  <?php echo "Post Display " + $post_meta_display; ?>
+  <input type="radio" name="scd-post-display" value="scd-post-display-above"
+    <?php echo $post_meta_display == 'scd-post-display-above' ? 'checked' : '' ?> />Above Post <br/>
+  <input type="radio" name="scd-post-display" value="scd-post-display-below"
+    <?php echo $post_meta_display == 'scd-post-display-below' ? 'checked' : '' ?> />Below Post <br />
+
+    <?php
+}
+
+
+
+/* Save the meta box's post metadata. */
+function scd_save_post_class_meta( $post_id, $post ) {
+
+  /* Verify the nonce before proceeding. */
+  if ( !isset( $_POST['scd-post-class-nonce'] ) || !wp_verify_nonce( $_POST['scd-post-class-nonce'], basename( __FILE__ ) ) )
+    return $post_id;
+
+  /* Get the post type object. */
+  $post_type = get_post_type_object( $post->post_type );
+
+  /* Check if the current user has permission to edit the post. */
+  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+    return $post_id;
+
+  /* Get the posted data and sanitize it for use as an HTML class. */
+  $new_scd_message = ( isset( $_POST['scd-post-message'] ) ? $_POST['scd-post-message'] : '' );
+  $new_scd_display = ( isset( $_POST['scd-post-display'] ) ? $_POST['scd-post-display'] : '' );
+
+  /* Get the meta value of the custom field key. */
+  $old_scd_message = get_post_meta( $post_id, 'scd-post-message', true );
+  $old_scd_display = get_post_meta( $post_id, 'scd-post-display', true );
+
+  /* If a new meta value was added and there was no previous value, add it. */
+  if ( $new_scd_message && '' == $old_scd_message ) {
+    add_post_meta( $post_id, 'scd-post-message', $new_scd_message, true );
+    add_post_meta( $post_id, 'scd-post-display', $new_scd_display, true );
+  }
+
+  /* If the new meta value does not match the old value, update it. */
+  elseif ( $new_scd_message && $new_scd_message != $old_scd_message ) {
+    update_post_meta( $post_id, 'scd-post-message', $new_scd_message );
+
+    if ($new_scd_display != $old_scd_display)
+      update_post_meta( $post_id, 'scd-post-display', $new_scd_display );
+  }
+
+  /* If there is no new meta value but an old value exists, delete it. */
+  elseif ( '' == $new_scd_message && $old_scd_message ) {
+    delete_post_meta( $post_id, 'scd-post-message', $old_scd_message );
+    delete_post_meta( $post_id, 'scd-post-display', $old_scd_display );
+  }
+}
